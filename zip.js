efe4129fde3f5e7e0afce7f1c49ca0014cb7a018
mauriciocap@gmail.com <mauriciocap@gmail.com>
2019-03-14 18:49:01 +0000

    FUNCIONAN CAMBIOS Modificamos para qe lea cachos de un zip (solo un archivo) y nos lo devuelva como ArrayBuffer

---
+++ zip.js
@@ -0,0 +1,55 @@
+var exec = cordova.require('cordova/exec');
+
+function newProgressEvent(result) {
+    var event = {
+            loaded: result.loaded,
+            total: result.total
+    };
+    return event;
+}
+
+exports.unzip = function(fileName, outputDirectory, callback, progressCallback) {
+    var win = function(result) {
+        if (result && typeof result.loaded != "undefined") {
+            if (progressCallback) {
+                return progressCallback(newProgressEvent(result));
+            }
+        } else if (callback) {
+            callback(0);
+        }
+    };
+    var fail = function(result) {
+        if (callback) {
+            callback(-1);
+        }
+    };
+    exec(win, fail, 'Zip', 'unzip', [fileName, outputDirectory]);
+};
+
+exports.unzip_str = function(fileName, compressedPath, callback) {
+    var win = function(result) {
+        if (callback) {
+            callback(result);
+        }
+    };
+    var fail = function(result) {
+        if (callback) {
+            callback(null, result);
+        }
+    };
+    exec(win, fail, 'Zip', 'unzip_str', [fileName, compressedPath]);
+};
+
+exports.unzip_dir = function(fileName, callback) {
+    var win = function(result) {
+        if (callback) {
+            callback(result);
+        }
+    };
+    var fail = function(result) {
+        if (callback) {
+            callback(null, result);
+        }
+    };
+    exec(win, fail, 'Zip', 'unzip_dir', [fileName]);
+};
